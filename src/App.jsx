eb0f8c178ea560fceefc6de9a5860de0b0249d3d
shivani-tyagi07 <shivanityagi1646@gmail.com>
2026-08-31 17:43:35 +0530

    app and home changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import {Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-const App= () =>{
-  return(
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
+
+const App = () => {
+  return (
     <div>
-      <Navbar/>
-      
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/experience" element={<Work />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
-  )
-}
-export default App
+  );
+};
+
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import About from '../components/About'
-import Skills from '../components/Skills'
-import Projects from '../components/Projects'
+import React from "react";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Skills from "../components/Skills";
+import Projects from "../components/Projects";
 import Work from "../components/Work";
 import Contact from "../components/Contact";
-import Footer  from "../components/Footer";
+import Footer from "../components/Footer";
+
 const Home = () => {
   return (
     <>
@@ -16,9 +17,8 @@ const Home = () => {
       <Work />
       <Contact />
       <Footer />
-     
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
